76872a1c5b56c13bf7fa5eb80bb17f245640dd8f
aaronprograms <aaronprograms@github.com>
2018-03-13 18:51:51 -0700

    Initial commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Aaron Son on 3/6/18.
+//  Copyright © 2018 Aaron Son. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
